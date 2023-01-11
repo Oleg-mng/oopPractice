3f16cbb45ac94118e6ab303fd27396f4723fba8f
oleg <legik7127@gmail.com>
2023-01-11 13:52:38 +0600

    Initial commit

---
+++ src/main/java/lecture_1/App1.java
@@ -0,0 +1,19 @@
+package lecture_1;
+
+public class App1 {
+    static double distance(Point2D a, Point2D b) {
+        return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
+    }
+
+    public static void main(String[] args) {
+    Point2D a = new Point2D(0, 2);
+    System.out.println(a);
+
+    Point2D b = new Point2D(10);
+    System.out.println(b);
+    System.out.println(distance(a, b));
+    }
+
+}
+
+
