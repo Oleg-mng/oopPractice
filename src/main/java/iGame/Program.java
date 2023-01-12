df7f216541f962b198e31fb56bf1ef09982cef1a
oleg <legik7127@gmail.com>
2023-01-12 22:47:37 +0600

    added methods with polymorphism

--- src/main/java/iGame/Program.java
+++ src/main/java/iGame/Program.java
@@ -2,9 +2,9 @@ package iGame;
 
 public class Program {
     public static void main(String[] args) {
-        Magician hero1 = new Magician();
+        BaseHero hero1 = new Magician();
         System.out.println(hero1.getInfo());
-        Priest hero2 = new Priest();
+        BaseHero hero2 = new Priest();
         System.out.println(hero2.getInfo());
         hero2.getDamage(hero1.attack());
         System.out.println("После атаки");
