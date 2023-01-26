91df661fa86a6b8d83b1a2096c70e92a153e1a6c
oleg <legik7127@gmail.com>
2023-01-26 15:39:27 +0600

    added method Abstraction with static fields

---
+++ src/main/java/Abstraction/Foo.java
@@ -0,0 +1,34 @@
+package Abstraction;
+// TODO: 20.01.2023
+
+public class Foo {
+    public Integer value;
+
+    public static Integer count;
+
+    public static Integer getCount(){
+        return count;
+    }
+
+
+    static {
+        count = 99999;
+    }
+
+    public Foo(){
+        //count++;
+    }
+
+    public void printCount(){
+        System.out.println(count);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
+
+
+
+
